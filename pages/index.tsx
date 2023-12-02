b316c592d6469c09cfceffa4ae9e7fe0a18e817f
Vinícius Yoda <vyoda4604@gmail.com>
2023-12-02 17:15:50 -0300

    more

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import Button from "@/components/Button/Button"
+import Switch from "@/components/Switch/Switch";
 
 const App = () => {
-  return <Button>Oi</Button>
-}
+  return (
+    <div className="h-screen mx-auto my-0 w-4/5 flex justify-center">
+      <div className="w-2/4 flex flex-col gap-1 justify-start">
+        <Switch />
+      </div>
+    </div>
+  );
+};
 
 export default App;
